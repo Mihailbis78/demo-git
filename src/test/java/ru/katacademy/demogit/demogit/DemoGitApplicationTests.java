8b9d57abcb4c9990b61cd27caadcb0a304d37b9b
mihailbystrov <mihailbis78@gmail.com>
2025-10-04 07:42:43 +0300

    init commit

---
+++ src/test/java/ru/katacademy/demogit/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.katacademy.demogit.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
